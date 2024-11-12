16ec5350efbae918a2a81cae20ff0dd341756d9e
Gabriel Zuco <brezoliin@gmail.com>
2024-11-11 23:15:59 -0300

    Formulario em funcionamento via emailjs

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Controller navbar sandwich
-document.addEventListener('DOMContentLoaded', function() {
-    const navbarLinks = document.querySelector('.navbar-links'); 
-    const navbarToggle = document.querySelector('.navbar-toggle'); 
-    const overlay = document.getElementById('overlay'); 
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarLinks = document.querySelector('.navbar-links');
+    const navbarToggle = document.querySelector('.navbar-toggle');
+    const overlay = document.getElementById('overlay');
 
-    window.toggleMenu = function() {
+    window.toggleMenu = function () {
         if (navbarLinks.classList.contains('show')) {
             navbarLinks.classList.remove('show');
             overlay.classList.remove('show');
@@ -16,9 +16,30 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    window.closeMenu = function() {
+    window.closeMenu = function () {
         navbarLinks.classList.remove('show');
         overlay.classList.remove('show');
         navbarToggle.innerHTML = '<i class="bi bi-list"></i>';
     }
+});
+
+// Emailjs config
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const templateParams = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    };
+
+    emailjs.send('service_wv4rgu3', 'template_cwi69ed', templateParams)
+    .then(function(response) {
+        console.log('Sucesso!', response.status, response.text);
+        alert('E-mail enviado com sucesso!');
+        document.getElementById('contactForm').reset();
+    }, function(error) {
+        console.error('Erro:', error);
+        alert('Erro ao enviar o e-mail, tente novamente.');
+    });
 });
